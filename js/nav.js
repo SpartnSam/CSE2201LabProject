c435d0cdc5d65e369c09d24841ad04095e542954
Nick Evelyn <nickevelyn7@gmail.com>
2022-06-03 17:16:40 -0400

    Modified the contact page

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+let navigationLinks = document.getElementById("nav-items");
+
+function openMenu() {
+    navigationLinks.style.right = "0";
+}
+
+function closeMenu() {
+    navigationLinks.style.right = "-250px";
+}
